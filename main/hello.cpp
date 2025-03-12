8124bfaf74144812121363b49b9bdb8b17fc56a1
Aakriti Mishra <111500356+Aakriti-2334@users.noreply.github.com>
2025-03-12 16:18:43 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, Aakriti\n";
+  cout << "Hello, PES2UG22CS004\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
