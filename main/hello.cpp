b5cef4df4973e10373ecab153a8d623eccc12039
Aakriti Mishra <111500356+Aakriti-2334@users.noreply.github.com>
2025-03-12 16:19:38 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, Aakriti\n";
+  cout << "Hello, Aakriti Mishra\n";
   cout << "Hello, PES2UG22CS004\n";
   cout << "I have successfully built and run this  \n";
   return 0;
